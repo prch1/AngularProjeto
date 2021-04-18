7fc8cdc12bcc0f5eea96ba93176ba75e0a3d5476
Paulo Ricardo <pauloricardoh@hotmail.com>
2021-04-18 17:33:12 -0300

    Aula 1 - Angular

---
+++ AluraPic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Aplicação';
+  description = 'Leão';
+  url = 'https://miro.medium.com/max/16510/1*jz-u2xRWZhN7Acy2W_Oyvw.jpeg';
+}
